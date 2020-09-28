5fcdabe6fca2d376458c839b143dc71774a6edc0
Роман Новиков <Demoi@rnovikov-pc.bsw.iron>
2020-09-28 08:09:11 +0300

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(names) {
+  if (!Array.isArray(names)) return false;
+  var scryptArray = [];
+  for (let i = 0; i < names.length; i++) {
+    if (typeof names[i] === "string") {
+      var split = names[i].toLowerCase().trim().split("");
+      scryptArray.push(split[0].toUpperCase());
+    }
+  }
+  scryptArray = scryptArray.sort(function (a, b) {
+    if (a > b) {
+      return 1;
+    }
+    if (b > a) {
+      return -1;
+    }
+    return 0;
+  });
+  return scryptArray.join("");
 };
