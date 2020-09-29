538abe16e0c883517dfdb24b32847d0f3fd34d96
Роман Новиков <Demoi@rnovikov-pc.bsw.iron>
2020-09-29 13:19:54 +0300

    feat: add Extended repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  var separator = "+";
+  var additionSeparator = "|";
+  var repeatTimes = 1;
+  var additionRepeatTimes = 1;
+  var result = "";
+  if (options.separator !== undefined) {
+    separator = options.separator;
+  }
+  if (options.repeatTimes !== undefined) {
+    repeatTimes = options.repeatTimes;
+  }
+  if (options.additionSeparator !== undefined) {
+    additionSeparator = options.additionSeparator;
+  }
+  for (let i = 0; i < repeatTimes; i++) {
+    if (result) {
+      result += separator;
+    }
+    result += str;
+    if (options.addition !== undefined) {
+      if (options.additionRepeatTimes !== undefined) {
+        additionRepeatTimes = options.additionRepeatTimes;
+      }
+      result += repeater(options.addition, {
+        repeatTimes: additionRepeatTimes,
+        separator: additionSeparator,
+      });
+    }
+  }
+  return result;
 };
-  
