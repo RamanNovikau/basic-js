50b20e05bd84dda8063a62dde9ea9d666614486b
Роман Новиков <Demoi@rnovikov-pc.bsw.iron>
2020-09-30 13:25:40 +0300

    fead: add Vigenere Cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,63 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  flag = true;
+  constructor(flag) {
+    if (typeof flag !== "undefined") {
+      this.flag = flag;
+    }
+  }
+
+  generateKey(string, key) {
+    var x = string.replace(/[^a-z]/gi, "").length;
+    for (let i = 0; ; i++) {
+      if (x == i) i = 0;
+      if (key.length >= x) break;
+      key += key.charAt(i);
+    }
+    return key;
+  }
+
+  encrypt(string, key) {
+    var encryptedString = "";
+    var uppertring = string.toUpperCase();
+    var upperKey = this.generateKey(uppertring, key).toUpperCase();
+    var k = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (uppertring.charCodeAt(i) >= 65 && uppertring.charCodeAt(i) <= 90) {
+        var char = (uppertring.charCodeAt(i) + upperKey.charCodeAt(i - k)) % 26;
+        char += "A".charCodeAt(0);
+        encryptedString += String.fromCharCode(char);
+      } else {
+        encryptedString += uppertring.charAt(i);
+        k++;
+      }
+    }
+    if (!this.flag) {
+      return encryptedString.split("").reverse().join("");
+    }
+    return encryptedString;
+  }
+  decrypt(string, key) {
+    var decryptedString = "";
+    var uppertring = string.toUpperCase();
+    var upperKey = this.generateKey(uppertring, key).toUpperCase();
+    var k = 0;
+    for (let i=0; i < string.length; i++) {
+      if (uppertring.charCodeAt(i) >= 65 && uppertring.charCodeAt(i) <= 90) {
+        var char =
+          (uppertring.charCodeAt(i) - upperKey.charCodeAt(i - k) + 26) % 26;
+        char += "A".charCodeAt(0);
+        decryptedString += String.fromCharCode(char);
+      } else {
+        decryptedString += uppertring.charAt(i);
+        k++;
+      }
+    }
+    if (!this.flag) {
+      return decryptedString.split("").reverse().join("");
+    }
+    return decryptedString;
   }
 }
 
